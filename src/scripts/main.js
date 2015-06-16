b70dd72280edfaf04eefc114a9c7653683d19574
Countrouble <131113042@students.mikroskil.ac.id>
2015-06-16 09:58:13 +0700

    perubahan Theme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  voltaire = require('bespoke-theme-voltaire'),
+  voltaire = require('bespoke-theme-nebula'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
